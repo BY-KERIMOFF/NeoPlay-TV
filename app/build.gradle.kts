992061cc8042cb3a7c77107df6d924e223949613
by-kerimoff <by-kerimoff@kali.by-kerimoff>
2026-07-25 21:36:20 +0300

    Update version to 1.2.5 (26) and increase logo size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.neoplay.tv"
         minSdk = 21
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.2.4"
+        versionCode = 26
+        versionName = "1.2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
